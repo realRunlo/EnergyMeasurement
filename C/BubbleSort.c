02ad36d3616e5985bf9b9813df330688a1d03205
Gonçalo Pereira <gteixeira.pereira@gmail.com>
2023-02-20 11:47:28 +0000

    BubbleSort implementations

---
+++ C/BubbleSort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int swapped = 0; // global variable to check if swap() function is called
+
+void swap(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+    swapped++;
+}
+
+int bubblesort(int *a, int size) // to demonstrate passing by refference in C (pointer variable receives only the base address of array)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+            swap(a+j, a+j+1); // making a function is good instead of many lines of code in main function 
+            }
+        }
+        if (swapped == 0)
+            return 1; // use return values better than break statement
+    }
+    return 0; // sorted 
+}
+
+int main(){
+    int n, i;
+    int *a = NULL; // security issue 
+    int size = 7;
+    int arr[7] = {64, 34, 25, 12, 22, 11, 90};
+
+    bubblesort(arr,size);
+    printf("\nSorted list:\n"); // Display the sorted array
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);// we're using C , so demonstrate pointers :) 
+    }
+    return 0;
+
+}
