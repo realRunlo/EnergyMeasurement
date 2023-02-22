f44cbeda5c5c7bbb17b028c5e4c30a03d8140d7f
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2023-02-22 18:32:08 +0000

    QuickSort

---
+++ C/QuickSort.c
@@ -0,0 +1,54 @@
+//Source ChatGPT
+#include <stdio.h>
+
+// function to swap two elements
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// function to partition the array
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // choose the last element as pivot
+    int i = (low - 1); // index of smaller element
+
+    for (int j = low; j <= high- 1; j++) {
+        // if current element is smaller than or equal to pivot
+        if (arr[j] <= pivot) {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// function to implement quicksort
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        // recursively sort elements before and after partition
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+// function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr, 0, n-1);
+    printf("Sorted array: ");
+    printArray(arr, n);
+    return 0;
+}
