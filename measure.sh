f3d546b9928e07645c54186fd1bb9622f43b6ad1
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2023-02-24 13:06:04 +0000

    Update shell loop

--- measure.sh
+++ measure.sh
@@ -1,15 +1,21 @@
+for size in 10 100 1000
+do
+    cd Utils/
+    python3 arrayUpdate.py $size ../Python/*.py ../C/*.c
+    cd ..
 
-cd C/
-make
-make measure 
+    cd C/
+    make
+    make measure 
 
-cat *.J | awk '!a[$0]++' > ../measurements.csv
-make clean
-cd ..
+    cat *.J | awk '!a[$0]++' > ../measurements.csv
+    make clean
+    cd ..
 
-cd Python/
-make measure 
+    cd Python/
+    make measure 
 
-cat *.J | awk '!a[$0]++' >> ../measurements.csv
-make clean
-cd ..
+    cat *.J | awk '!a[$0]++' >> ../measurements.csv
+    make clean
+    cd ..
+done
