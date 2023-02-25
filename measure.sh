38f4431cf9e72afaa95d8974d1bb26fd0181132b
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2023-02-25 23:15:15 +0000

    csv update format

--- measure.sh
+++ measure.sh
@@ -1,33 +1,39 @@
+#!/bin/bash
+
+echo "Size ,Language ,Program ,Package ,Core(s) ,GPU ,DRAM? ,Time (ms)" > measurements.csv
+
+# Loop over size values
 for size in 10 100 1000
 do
+    # Update input arrays with new size
     cd Utils/
     python3 arrayUpdate.py $size ../Python/*.py ../C/*.c
     cd ..
 
+    # Build and measure C programs
     cd C/
     make
     make measure 
 
-    cat *.J > ../measurements.csv
+    # Append C measurement results to CSV file with size column
+    for file in *.J;
+        do tail -n +2 -q "$file" | sed "s/^/$size,C,/" | sed "s/C_//" >> ../measurements.csv; 
+    done
     make clean
     cd ..
 
+    # Build and measure Python programs
     cd Python/
     make measure 
 
-    cat *.J >> ../measurements.csv
+    # Append Python measurement results to CSV file with size column
+    for file in *.J;
+        do tail -n +2 -q "$file" | sed "s/^/$size,Python,/" | sed "s/.py//" >> ../measurements.csv; 
+    done
     make clean
     cd ..
 
 done
 
-# Remove linhas repetidas
-awk '!a[$0]++' measurements.csv > tmp.csv
-mv tmp.csv measurements.csv
-       
-# Adiciona título no início da linha       
-sed -i '1s/^/Linguagem, /' measurements.csv
-
-# Coloca todos os nomes dos programas para o formato "[Linguagem]_[Algoritmo de ordenação]"
-sed -E 's/([a-zA-Z]+)\.([a-zA-Z]+) ,/\2 , \1 ,/g; s/([a-zA-Z]+)_([a-zA-Z]+) ,/\1 , \2 ,/g' measurements.csv > tmp.csv
+awk -F',' 'BEGIN{OFS=","} {print $2, $3, $1, $4, $5, $6, $7, $8}' measurements.csv > tmp.csv
 mv tmp.csv measurements.csv
