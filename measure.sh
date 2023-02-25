e326ba291523d1bc60d78a7ad1c5d201cd22b699
Simão Pedro Sá Cunha <simaopscunha@outlook.pt>
2023-02-25 21:14:05 +0000

    csv com coluna da linguagem

--- measure.sh
+++ measure.sh
@@ -1,4 +1,4 @@
-for size in 10 #100 1000
+for size in 10 100 1000
 do
     cd Utils/
     python3 arrayUpdate.py $size ../Python/*.py ../C/*.c
@@ -18,9 +18,16 @@ do
     cat *.J >> ../measurements.csv
     make clean
     cd ..
+
 done
 
 # Remove linhas repetidas
 awk '!a[$0]++' measurements.csv > tmp.csv
 mv tmp.csv measurements.csv
        
+# Adiciona título no início da linha       
+sed -i '1s/^/Linguagem, /' measurements.csv
+
+# Coloca todos os nomes dos programas para o formato "[Linguagem]_[Algoritmo de ordenação]"
+sed -E 's/([a-zA-Z]+)\.([a-zA-Z]+) ,/\2 , \1 ,/g; s/([a-zA-Z]+)_([a-zA-Z]+) ,/\1 , \2 ,/g' measurements.csv > tmp.csv
+mv tmp.csv measurements.csv
