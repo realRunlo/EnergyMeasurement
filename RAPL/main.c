80d365c1bdbf8d4cfa700b7241421efb00292de0
Simão Pedro Sá Cunha <simaopscunha@outlook.pt>
2023-05-04 10:20:00 +0100

    fix no tempo de execução e agora o python compilado é mais rapido que o interpretado

--- RAPL/main.c
+++ RAPL/main.c
@@ -1,10 +1,9 @@
 
 /*
  *  Análise e Teste de Software
- *  João Saraiva 
+ *  João Saraiva
  *  2016-2017
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,187 +14,193 @@
 #include "rapl.h"
 #include "sensors.h"
 
-#define TEMPERATURETHRESHOLD 42.16666793823242
+#define TEMPERATURETHRESHOLD 41.0
 #define SHORTWATTS 10.0
 #define SHORTTIME 0.001
 #define LONGWHATTS 5.0
 #define LONGTIME 10.0
 #define RUNTIME
 
-long fib(int n){
-  if(n<1)
-    return 1;
-  else return fib(n-1) + fib(n-2);
+long fib(int n)
+{
+    if (n < 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
 }
 
-int main (int argc, char **argv) 
-{ char command[500],res[500];
-  int  ntimes = 1;
-  int  core = 0;
-  int  i=0;
+int main(int argc, char **argv)
+{
+    char command[500], res[500];
+    int ntimes = 1;
+    int core = 0;
+    int i = 0;
 
 #ifdef RUNTIME
-  clock_t begin, end;
-  double time_spent;
+    clock_t begin, end;
+    double time_spent;
 
-  struct timeval tvb,tva;
-  
+    struct timeval tvb, tva;
+
 #endif
-  
-  FILE * fp;
 
-  //RAPLCAP
-  raplcap rc;
-  raplcap_limit rl_short, rl_long;
-  uint32_t q, j, n, d;
-  
+    FILE *fp;
 
-  // initialize
-  if (raplcap_init(&rc)) {
-    perror("raplcap_init");
-    return -1;
-  }
-  
-  // get the number of RAPL packages
-  n = raplcap_get_num_packages(NULL);
-  if (n == 0) {
-    perror("raplcap_get_num_packages");
-    return -1;
-  }
+    // RAPLCAP
+    raplcap rc;
+    raplcap_limit rl_short, rl_long;
+    uint32_t q, j, n, d;
 
-  // assuming each package has the same number of die, only querying for package=0
-  d = raplcap_get_num_die(&rc, 0);
-  if (d == 0) {
-    perror("raplcap_get_num_die");
-    raplcap_destroy(&rc);
-    return -1;
-  }
-
-  // for each package die, set a power cap of 100 Watts for short_term and 50 Watts for long_term constraints
-  // a time window of 0 leaves the time window unchanged
-  rl_short.watts = SHORTWATTS;
-  rl_short.seconds = SHORTTIME;
-  rl_long.watts = LONGWHATTS;
-  rl_long.seconds = LONGTIME;
-  for (q = 0; q < n; q++) {
-    for (j = 0; j < d; j++) {
-      if (raplcap_pd_set_limits(&rc, q, j, RAPLCAP_ZONE_PACKAGE, &rl_long, &rl_short)) {
-        perror("raplcap_pd_set_limits");
-      }
-    }
-  }
-
-  // for each package die, enable the power caps
-  // this could be done before setting caps, at the risk of enabling unknown power cap values first
-  for (q = 0; q < n; q++) {
-    for (j = 0; j < d; j++) {
-      if (raplcap_pd_set_zone_enabled(&rc, q, j, RAPLCAP_ZONE_PACKAGE, 1)) {
-        perror("raplcap_pd_set_zone_enabled");
-      }
-    }
-  }
-  //RAPLCAP
-
-
-
-
-
-  // printf("Program to be executed: %d",argc);
-  //strcpy( command, "./" );
- strcat(command,argv[1]);
- printf("Program to be executed: %s\n",argv[1]);
-
-  strcpy(command, "./" );
-  strcat(command,argv[1]);
-
-  ntimes = atoi (argv[2]);
-
-  strcpy(res,command);
-  strcat(res,".J");
-  printf("Command: %s  %d-times res: %s\n",command,ntimes,res);
-  
-
-  printf("\n\n RUNNING THE PARAMETRIZED PROGRAM:  %s\n\n\n",command);
-  fflush(stdout);
-  
-  fp = fopen(res,"w");
-  rapl_init(core);
-
-  fprintf(fp,"Program, Package , Core(s) , GPU , DRAM? , Time (ms) , Temperature , Memory \n");
-
-  int maxTrys = 5;
-  float temperature;
-  char str_temp[20];
-  
-  for (i = 0 ; i < ntimes ; i++)
+    // initialize
+    if (raplcap_init(&rc))
     {
-      temperature = getTemperature();
-      /*
-      for (int currentTrys = 0; currentTrys < maxTrys && temperature>TEMPERATURETHRESHOLD; currentTrys++,temperature = getTemperature()) //NEW
+        perror("raplcap_init");
+        return -1;
+    }
+
+    // get the number of RAPL packages
+    n = raplcap_get_num_packages(NULL);
+    if (n == 0)
+    {
+        perror("raplcap_get_num_packages");
+        return -1;
+    }
+
+    // assuming each package has the same number of die, only querying for package=0
+    d = raplcap_get_num_die(&rc, 0);
+    if (d == 0)
+    {
+        perror("raplcap_get_num_die");
+        raplcap_destroy(&rc);
+        return -1;
+    }
+
+    // for each package die, set a power cap of 100 Watts for short_term and 50 Watts for long_term constraints
+    // a time window of 0 leaves the time window unchanged
+    rl_short.watts = SHORTWATTS;
+    rl_short.seconds = SHORTTIME;
+    rl_long.watts = LONGWHATTS;
+    rl_long.seconds = LONGTIME;
+    for (q = 0; q < n; q++)
+    {
+        for (j = 0; j < d; j++)
         {
-          printf("Sleeping\n");
-          sleep(1);
-        }*/
+            if (raplcap_pd_set_limits(&rc, q, j, RAPLCAP_ZONE_PACKAGE, &rl_long, &rl_short))
+            {
+                perror("raplcap_pd_set_limits");
+            }
+        }
+    }
+
+    // for each package die, enable the power caps
+    // this could be done before setting caps, at the risk of enabling unknown power cap values first
+    for (q = 0; q < n; q++)
+    {
+        for (j = 0; j < d; j++)
+        {
+            if (raplcap_pd_set_zone_enabled(&rc, q, j, RAPLCAP_ZONE_PACKAGE, 1))
+            {
+                perror("raplcap_pd_set_zone_enabled");
+            }
+        }
+    }
+    // RAPLCAP
+
+    // printf("Program to be executed: %d",argc);
+    // strcpy( command, "./" );
+    strcat(command, argv[1]);
+    printf("Program to be executed: %s\n", argv[1]);
+
+    strcpy(command, "./");
+    strcat(command, argv[1]);
+
+    ntimes = atoi(argv[2]);
+
+    strcpy(res, command);
+    strcat(res, ".J");
+    printf("Command: %s  %d-times res: %s\n", command, ntimes, res);
+
+    printf("\n\n RUNNING THE PARAMETRIZED PROGRAM:  %s\n\n\n", command);
+    fflush(stdout);
+
+    fp = fopen(res, "w");
+    rapl_init(core);
+
+    fprintf(fp, "Program, Package , Core(s) , GPU , DRAM? , Time (ms) , Temperature , Memory \n");
+
+    int maxTrys = 5;
+    float temperature;
+    char str_temp[20];
+
+    for (i = 0; i < ntimes; i++)
+    {
+        temperature = getTemperature();
+        
+        for (int currentTrys = 0; currentTrys < maxTrys && temperature>TEMPERATURETHRESHOLD; currentTrys++,temperature = getTemperature()) //NEW
+          {
+            printf("Sleeping\n");
+            sleep(1);
+          }
         sprintf(str_temp, "%.1f", temperature);
 
-        fprintf(fp,"%s , ",argv[1]);
+        fprintf(fp, "%s , ", argv[1]);
+
+        rapl_before(fp, core);
+
         
-        rapl_before(fp,core);
-      
-     
-      char cmd[1024];
-      int mem;
-      // Build command to execute, this command measures memory too
-      sprintf(cmd, "{ /usr/bin/time -v %s > /dev/null; } 2>&1 | grep 'Maximum resident' | sed 's/[^0-9]\\+\\([0-9]\\+\\).*/\\1/'", command);
-    
+
+        //////////////////////////////////////////////////////////////
 #ifdef RUNTIME
         begin = clock();
-	gettimeofday(&tvb, 0);
+        gettimeofday(&tvb, 0);
 #endif
+        ///////////////////Add memory column in csv///////////////////
+        char cmd[1024];
+        int mem;
+        sprintf(cmd, "{ /usr/bin/time -v %s > /dev/null; } 2>&1 | grep 'Maximum resident' | sed 's/[^0-9]\\+\\([0-9]\\+\\).*/\\1/'", command);
 
- FILE* fp2 = popen(cmd, "r");
-      if (fp2 == NULL) {
-          fprintf(stderr, "Error running command\n");
-          exit(-1);
-    }
+        FILE *fp2 = popen(cmd, "r");
+        if (fp2 == NULL)
+        {
+            fprintf(stderr, "Error running command\n");
+            exit(-1);
+        }
 
+        char buf[1024];
+        while (fgets(buf, sizeof(buf), fp2) != NULL)
+        {
+        }
+        pclose(fp2);
+
+        mem = atoi(buf);
 
 #ifdef RUNTIME
 
-	end = clock();
-	gettimeofday(&tva, 0);
-	//	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	time_spent = ((tva.tv_sec-tvb.tv_sec)*1000000 + tva.tv_usec-tvb.tv_usec)/1000;
+        end = clock();
+        gettimeofday(&tva, 0);
+        //	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        time_spent = ((tva.tv_sec - tvb.tv_sec) * 1000000 + tva.tv_usec - tvb.tv_usec) / 1000;
 #endif
-	rapl_after(fp,core);
-   ///////////////////Add memory column in csv///////////////////
-  char buf[1024];
-  while (fgets(buf, sizeof(buf), fp2) != NULL){}
-  pclose(fp2);
-    
-  mem = atoi(buf);
 
-#ifdef RUNTIME	
-	fprintf(fp,"%G ,",time_spent);
-  fprintf(fp, " %s ,", str_temp);
-  fprintf(fp, " %d \n", mem);
+        rapl_after(fp, core);
 
+#ifdef RUNTIME
+        fprintf(fp, "%G ,", time_spent);
+        fprintf(fp, " %s ,", str_temp);
+        fprintf(fp, " %d \n", mem);
 
-#endif	
+#endif
     }
-    
-  printf("\n\n END of PARAMETRIZED PROGRAM: \n");
 
-  fclose(fp);
-  fflush(stdout);
+    printf("\n\n END of PARAMETRIZED PROGRAM: \n");
 
+    fclose(fp);
+    fflush(stdout);
 
-  // cleanup
-  if (raplcap_destroy(&rc)) {
-    perror("raplcap_destroy");
-  }
-  return 0;
+    // cleanup
+    if (raplcap_destroy(&rc))
+    {
+        perror("raplcap_destroy");
+    }
+    return 0;
 }
-
-
-
